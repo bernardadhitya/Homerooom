c1627ae33cd3b6b56bbc47705fd06c6bb29e7a9a
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-11-07 11:48:11 +0700

    [Bernard] Use SafeAreaView to prevent components collision

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
