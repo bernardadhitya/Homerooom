867b427bc914a9ba28ab6be5959be2b1430e337a
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-11-14 21:10:48 +0700

    [Bernard] Add color and avatar constants

---
+++ src/Constants/Avatars.js
@@ -0,0 +1,14 @@
+export const Avatars = {
+  dog: '🐶',
+  cat: '🐱',
+  rat: '🐭',
+  hamster: '🐹',
+  rabbit: '🐰',
+  fox: '🦊',
+  bear: '🐻',
+  panda: '🐼',
+  tiger: '🐯',
+  lion: '🦁',
+  penguin: '🐧',
+  monkey: '🐵',
+}
---
+++ src/Constants/Colors.js
@@ -0,0 +1,12 @@
+export const Colors = {
+  red: '#EF5B54',
+  yellow: '#FDD444',
+  green: '#9DCB37',
+  aqua: '#99E1DD',
+  blue: '#63C7FD',
+  lightRed: '#FFE9E5',
+  lightYellow: '#FFF4CD',
+  lightGreen: '#F2FFD7',
+  lightAqua: '#F2FFD7',
+  lightBlue: '#BDE7FD'
+}
